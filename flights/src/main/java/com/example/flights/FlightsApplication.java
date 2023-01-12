982f93991ea7cc201a0c4d9e6f551919797d0068
Yoana Slavcheva <yoanaslavchevaa@gmail.com>
2023-01-12 13:42:14 +0200

    Conected bd and created post request

--- flights/src/main/java/com/example/flights/FlightsApplication.java
+++ flights/src/main/java/com/example/flights/FlightsApplication.java
@@ -2,17 +2,14 @@ package com.example.flights;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-@EntityScan(basePackages= {"com.example.flights.entity"})
-@ComponentScan(basePackages = {"com.example.flights.entity"})
-@EnableJpaRepositories
+
 @SpringBootApplication
+@EnableJpaRepositories
 public class FlightsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FlightsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FlightsApplication.class, args);
+    }
 
 }
